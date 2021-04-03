e94bb1bc03659626f4775aa30936780d0df110be
mohamed <mohammed.elkhazen@gmail.com>
2021-04-03 09:44:26 -0500

    update

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -4,6 +4,7 @@ export class Satellite {
     type: string;
     operational: boolean;
     launchDate: string;
+  static shouldSouwWarning: any;
     constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean){
     this.name = name;
     this.type = type;
@@ -11,4 +12,12 @@ export class Satellite {
     this.orbitType = orbitType;
     this.operational = operational;
     }
+     shouldShowWarning (): boolean {
+        if (this.type === 'Space Debris'){
+            return true
+        }else{
+            return false
+        }
+    }
 }
+console.log(Satellite.shouldShowWarning())
