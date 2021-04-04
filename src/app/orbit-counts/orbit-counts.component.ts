808918c93a0f7e7483e0ef61bf4df53441b84ed4
mohamed <mohammed.elkhazen@gmail.com>
2021-04-03 19:39:48 -0500

    update

--- src/app/orbit-counts/orbit-counts.component.ts
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,Input, OnInit } from '@angular/core';
+import { Satellite } from '../satellite';
 
 @Component({
   selector: 'app-orbit-counts',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./orbit-counts.component.css']
 })
 export class OrbitCountsComponent implements OnInit {
+  @Input() satellites: Satellite[];
 
   constructor() { }
 
